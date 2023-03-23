f595b5bb414e1f349322a0b48fe517b0b6157362
Fernando Abreu <fas.abreu@gmail.com>
2023-03-23 08:23:16 -0300

    Ajustes

---
+++ LanchesMac/Controllers/LancheController.cs
@@ -0,0 +1,21 @@
+﻿using LanchesMac.Repositoreis.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _LancheRepository;
+
+        public LancheController(ILancheRepository lancheRepository)
+        {
+            _LancheRepository = lancheRepository;
+        }
+
+        public IActionResult List()
+        {
+            var lanches = _LancheRepository.Lanches;
+            return View(lanches);
+        }
+    }
+}
