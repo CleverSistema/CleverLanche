6cdc2792c0f7f5ba98542b01397a1f92f1452f56
Fernando Abreu <fabreu@narwalsistemas.com.br>
2023-05-05 08:47:30 -0300

    Implementação de novas Funcionalizades

--- LanchesMac/Controllers/HomeController.cs
+++ LanchesMac/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+using LanchesMac.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,9 +8,20 @@ namespace LanchesMac.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILancheRepository _repository;
+
+        public HomeController(ILancheRepository repository)
+        {
+            _repository = repository;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var homeViewModel = new HomeViewModel
+            {
+                LanchesPreferidos = _repository.LanchesPreferidos
+            };
+            return View(homeViewModel);
         }
 
 
